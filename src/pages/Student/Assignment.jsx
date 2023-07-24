08688fc7fbd3144b9df79d66ca6c58695ea1a1e9
GhulamMurtazaButt <buttmurtaza01@gmail.com>
2023-07-25 00:53:47 +0500

    assignment page

--- src/pages/Student/Assignment.jsx
+++ src/pages/Student/Assignment.jsx
@@ -1,12 +1,17 @@
 import { Stack } from "@chakra-ui/react";
 import StudentDashboardDetail from "../../components/pages/Student/StudentDashboardDetail";
-import StudentDashboardTable from "../../components/pages/Student/StudentDashboardTable";
+import StudentAssignmentTable from "../../components/pages/Student/StudentAssignmentTable";
+
+const tableHeader = ["TITLE", "START DATE", "END DATE", "DOWNLOAD", "UPLOAD", "STATUS"];
+const tableData = [
+  ["Assignment 01", "1/01/2023 1:00 PM", "10/01/2023 1:00 AM", "file01.pdf", null, null]
+]
 
 export default function Assignment() {
   return (
     <Stack minW="100%">
       <StudentDashboardDetail text={"Class Assignments"} />
-      <StudentDashboardTable />
+      <StudentAssignmentTable header={tableHeader} data={tableData}/>
     </Stack>
   );
 }
