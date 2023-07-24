7b4cccf6365e0fde4574bc0d7771213bea237b54
GhulamMurtazaButt <buttmurtaza01@gmail.com>
2023-07-24 12:28:57 +0500

    some changes

--- src/pages/Student/Home.jsx
+++ src/pages/Student/Home.jsx
@@ -2,14 +2,17 @@ import {Stack} from "@chakra-ui/react";
 import StudentSidebarwithHeader from "../../components/common/Sidebar/StudentSidebarwithHeader";
 import StudentDashboardDetail from "../../components/common/Sidebar/StudentDashboardDetail";
 import StudentDashboardTable from "../../components/common/Sidebar/StudentDashboardTable";
-
-
-export default function StudentDashboard() {
+const headers = ["COURSE ID", "COURSE NAME", "TEACHER NAME", "CLASS BATCH", "ATTENDANCE"];
+const tableData = [
+  ["PSI-202", "Computer Skills", "Rana Tahir", "Batch-09", "100%"],
+  ["PSI-203", "IT Information", "Murtaza", "Batch-09", "60%"],
+];
+export default function Home() {
   return (
     <Stack minW="100%">
       <StudentSidebarwithHeader>
         <StudentDashboardDetail text={"Registered Course List"}/>
-        <StudentDashboardTable />
+        <StudentDashboardTable headers={headers} data={tableData}/>
             </StudentSidebarwithHeader>
     </Stack>
   );
