6ba27a09943ab51971a2589322e4dfad02640a23
Saqib Ansari <saqib.ns111@gmail.com>
2023-09-19 17:06:15 +0500

    add attendence page responsive

--- src/pages/Teacher/AddAttendence.jsx
+++ src/pages/Teacher/AddAttendence.jsx
@@ -12,8 +12,11 @@ import {
   Progress,
   Text,
   Flex,
+  IconButton,
+  TableContainer,
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import { FaArrowLeft, FaArrowRight, FaCheck, FaTimes } from "react-icons/fa"; // Import icons
 
 const studentData = [
   {
@@ -104,7 +107,6 @@ const studentData = [
 ];
 
 const studentsPerPage = 10; // Number of students per page
-
 const AddStudentAttendancePage = () => {
   const [students, setStudents] = useState(studentData);
   const { courseId } = useParams();
@@ -152,83 +154,95 @@ const AddStudentAttendancePage = () => {
 
   // Get the students for the current page
   const studentsToShow = students.slice(startIndex, endIndex);
-
   return (
-    <Box p={4} minW={"100%"}>
-      <Heading size="lg" mb={4}>
+    <Box p={[2, 4, 6, 8]} minW={"100%"}>
+      <Heading size="lg" mb={[2, 4]}>
         {courseId} - Student Attendance
       </Heading>
 
-      <Text fontSize="lg" mb={4}>
+      <Text fontSize="lg" mb={[2, 4]}>
         Current Page: {currentPage + 1} {/* Display the current page number */}
       </Text>
 
-      <Flex justify="space-between" mb={10}>
-        <Button colorScheme="green" size="md" onClick={markAllPresent}>
+      <Flex
+        direction={["column", "row"]}
+        justify="space-between"
+        mb={[4, 10]}
+        align={["center", "initial"]}
+      >
+        <Button
+          colorScheme="green"
+          size={["sm", "md"]}
+          onClick={markAllPresent}
+        >
           Mark All Present
         </Button>
-        <Button
+        <IconButton
           bgColor={"primary.base"}
           color={"white"}
           _hover={{ bgColor: "primary.hover", color: "white.base" }}
-          size="md"
-        >
-          Submit
-        </Button>
-        <Button colorScheme="red" size="md" onClick={markAllAbsent}>
+          size={["sm", "md"]}
+          mt={[2, 0]}
+          ml={[0, 2]}
+          mb={[2, 0]}
+          icon={<FaCheck />} // Add the icon here
+        ></IconButton>
+        <Button colorScheme="red" size={["sm", "md"]} onClick={markAllAbsent}>
           Mark All Absent
         </Button>
       </Flex>
-
-      <Table variant="striped">
-        <Thead>
-          <Tr>
-            <Th>Roll No.</Th>
-            <Th>Name</Th>
-            <Th>Total Attendance</Th>
-            <Th>Attendance</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {studentsToShow.map((student) => (
-            <Tr key={student.id}>
-              <Td>{student.rollNo}</Td>
-              <Td>{student.name}</Td>
-              <Td>
-                {student.totalAttendance}{" "}
-                <Progress
-                  size="sm"
-                  value={(student.totalAttendance / 100) * 100}
-                  color={student.totalAttendance < 80 ? "red" : "green"}
-                />
-              </Td>
-              <Td>
-                <Button
-                  colorScheme={student.isPresent ? "green" : "red"}
-                  size="sm"
-                  onClick={() => handleMarkAttendance(student.id)}
-                >
-                  {student.isPresent ? "Present" : "Absent"}
-                </Button>
-              </Td>
+      <TableContainer>
+        <Table variant="striped">
+          <Thead>
+            <Tr>
+              <Th>Roll No.</Th>
+              <Th>Name</Th>
+              <Th>Total Attendance</Th>
+              <Th>Attendance</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
-
+          </Thead>
+          <Tbody>
+            {studentsToShow.map((student) => (
+              <Tr key={student.id}>
+                <Td>{student.rollNo}</Td>
+                <Td>{student.name}</Td>
+                <Td>
+                  {student.totalAttendance}
+                  <Progress
+                    size="sm"
+                    value={(student.totalAttendance / 100) * 100}
+                    colorScheme={student.totalAttendance < 80 ? "red" : "green"}
+                    height="10px" // Adjust the height as needed
+                    borderRadius="10px" // Adjust the border radius as needed
+                  />
+                </Td>
+                <Td>
+                  <IconButton
+                    colorScheme={student.isPresent ? "green" : "red"}
+                    size="sm"
+                    onClick={() => handleMarkAttendance(student.id)}
+                    aria-label={student.isPresent ? "Present" : "Absent"}
+                    icon={student.isPresent ? <FaCheck /> : <FaTimes />}
+                  />
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
       <Flex justify="space-between">
-        <Button
+        <IconButton
           isDisabled={currentPage === 0}
           onClick={() => setCurrentPage(currentPage - 1)}
-        >
-          Previous Page
-        </Button>
-        <Button
+          aria-label="Previous Page"
+          icon={<FaArrowLeft />}
+        />
+        <IconButton
           isDisabled={endIndex >= students.length}
           onClick={() => setCurrentPage(currentPage + 1)}
-        >
-          Next Page
-        </Button>
+          aria-label="Next Page"
+          icon={<FaArrowRight />}
+        />
       </Flex>
     </Box>
   );
