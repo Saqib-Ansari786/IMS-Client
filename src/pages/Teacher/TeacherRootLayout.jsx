6e07206480aa235436100833ed26394af6551932
Saqib Ansari <saqib.ns111@gmail.com>
2023-10-04 22:48:42 +0500

    added in sidebar

--- src/pages/Teacher/TeacherRootLayout.jsx
+++ src/pages/Teacher/TeacherRootLayout.jsx
@@ -6,7 +6,7 @@ const LinkItems = [
   { name: "Home", icon: InfoIcon, route: "/teacher" },
   { name: "Attendance", icon: SearchIcon, route: "/teacher/manage-attendance" },
   { name: "Assignments", icon: StarIcon, route: "/teacher/assignments" },
-  { name: "Marks Summary", icon: SettingsIcon, route: "/teacher/marks" },
+  { name: "Marks Summary", icon: SettingsIcon, route: "/teacher/classes" },
   {
     name: "Course Resources",
     icon: SettingsIcon,
