e45fdbce4f3558087dd72934b1023d04ca0bd6c1
GhulamMurtazaButt <buttmurtaza01@gmail.com>
2023-10-11 14:02:19 +0500

    some changing

--- src/components/pages/Admin/ShowEntriesDropdown.jsx
+++ src/components/pages/Admin/ShowEntriesDropdown.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Select } from '@chakra-ui/react';
+import React from "react";
+import { Box, Flex, Select } from "@chakra-ui/react";
 
 const ShowEntriesDropdown = ({ entries, setEntries }) => {
   const options = [5, 10, 25, 50, 100];
 
   return (
-    <Select
-      value={entries}
-      onChange={(e) => setEntries(parseInt(e.target.value))}
-    >
-      {options.map((option) => (
-        <option key={option} value={option}>
-          {option}
-        </option>
-      ))}
-    </Select>
+    <Flex ml={5} mt={5} alignItems="center">
+      <Box mr={2}>Show</Box>
+      <Select
+        w={85}
+        value={entries}
+        onChange={(e) => setEntries(parseInt(e.target.value))}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </Select>
+      <Box ml={2}>entries</Box>
+    </Flex>
   );
 };
 
