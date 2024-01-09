eefe96bcf64e21ae6077b1509e8981687c64da02
Saqib Ali <saqib.ns111@gmail.com>
2024-01-10 02:32:38 +0500

    update

--- src/components/pages/Student/ViewLibraryTable.jsx
+++ src/components/pages/Student/ViewLibraryTable.jsx
@@ -1,75 +1,3 @@
-// import { useState } from "react";
-// import { Table, Thead, Tbody, Tr, Th, Td, TableContainer, Input, Button, Box } from "@chakra-ui/react";
-// import { set } from "date-fns";
-
-  
-//   export default function ViewLibraryTable({ headers, data }) {
-//     const [search, setSearch] = useState("");
-
-  
-//     return (
-//       <TableContainer bgColor={"white"}>
-//         <Box mb={4} bg="white" p={2} display="flex" alignItems="center">
-//         <Input
-//           placeholder="Search by title..."
-//           value={search}
-//           onChange={(e) => setSearch(e.target.value)}
-//         />
-//         <Button ml={2} colorScheme="blue" >
-//           Search
-//         </Button>
-//       </Box>
-//         <Table variant="simple" backgroundColor={"white"} mt={3} borderRadius={8}>
-//           <Thead>
-//             <Tr>
-//               {headers &&
-//                 headers.map((header, index) => (
-//                   <Th key={index} textAlign="center">
-//                     {header}
-//                   </Th>
-//                 ))}
-//             </Tr>
-//           </Thead>
-//           <Tbody>
-//             {data &&
-//               data.filter((row)=> {
-//                 return search.toLowerCase() === ''
-//                 ? row
-//                 : row.title.toLowerCase().includes(search) ;
-//               }).map((row, rowIndex) => (
-//                 <Tr key={rowIndex}>
-//                   <Td key={rowIndex} textAlign="center">
-//                     {row.title}
-//                   </Td>
-//                   <Td key={rowIndex} textAlign="center">
-//                    {row.autherName}
-//                   </Td>
-//                   <Td key={rowIndex} textAlign="center">
-//                     {row.publisherName}
-//                   </Td>
-//                   <Td key={rowIndex} textAlign="center">
-//                     {row.isbn}
-//                   </Td>
-//                   <Td key={rowIndex} textAlign="center">
-//                     {row.category}
-//                   </Td>
-//                   <Td key={rowIndex} textAlign="center">
-//                   <span
-//                       style={{
-//                         color: row.availability === "Available" ? "green" : "red",
-//                       }}
-//                     >
-//                       {row.availability}
-//                     </span>
-//                   </Td>
-//                 </Tr>
-//               ))}
-//           </Tbody>
-//         </Table>
-//       </TableContainer>
-//     );
-//   }
-  
 import React, { useState } from "react";
 import {
   Table,
@@ -84,13 +12,43 @@ import {
   Box,
   Badge,
 } from "@chakra-ui/react";
+import { selectUser } from "../../../store/redux-slices/user_slice";
+import { useSelector } from "react-redux";
+import apiMiddleware from "../../common/Server/apiMiddleware";
 
 export default function ViewLibraryTable({ headers, data }) {
   const [search, setSearch] = useState("");
+  const user = useSelector(selectUser);
 
-  const handleIssueRequest = (book) => {
+  console.log(user);
+
+  const handleIssueRequest = async (book) => {
     // Add logic here to handle issuing the book
-    console.log(`Issuing request for ${book.title}`);
+    console.log(`Issuing request for ${book._id}`);
+
+    const issueRequest = {
+      libraryItemId: book._id,
+      studentId: user.id,
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(issueRequest),
+    };
+
+    const response = await apiMiddleware(
+      "library/library-items/issue-request",
+      requestOptions
+    );
+
+    console.log(response);
+
+    if (response.success) {
+      alert("Issue request sent successfully!");
+    } else {
+      alert("Error sending issue request!");
+    }
   };
 
   return (
@@ -118,13 +76,13 @@ export default function ViewLibraryTable({ headers, data }) {
         </Thead>
         <Tbody>
           {data &&
-            data.map((row, rowIndex) => (
-              <Tr key={rowIndex}>
+            data?.map((row, rowIndex) => (
+              <Tr key={row._id}>
                 <Td key={rowIndex} textAlign="center">
                   {row.title}
                 </Td>
                 <Td key={rowIndex} textAlign="center">
-                  {row.autherName}
+                  {row.authorName}
                 </Td>
                 <Td key={rowIndex} textAlign="center">
                   {row.publisherName}
@@ -138,14 +96,14 @@ export default function ViewLibraryTable({ headers, data }) {
                 <Td key={rowIndex} textAlign="center">
                   <span
                     style={{
-                      color: row.availability === "Available" ? "green" : "red",
+                      color: row.availability === true ? "green" : "red",
                     }}
                   >
-                    {row.availability}
+                    {row.availability === true ? "Available" : "Not Available"}
                   </span>
                 </Td>
                 <Td key={rowIndex} textAlign="center">
-                  {row.availability === "Available" ? (
+                  {row.availability === true ? (
                     <Button
                       size="sm"
                       colorScheme="blue"
