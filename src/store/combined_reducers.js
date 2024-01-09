cc38517978d5ee8f87f085e3fc37bb0ee9ef2ec9
Saqib Ali <saqib.ns111@gmail.com>
2024-01-10 03:43:27 +0500

    update

--- src/store/combined_reducers.js
+++
@@ -1,9 +0,0 @@
-import teacher_slice from "./redux-slices/teacher_slice";
-import user_slice from "./redux-slices/user_slice";
-
-const rootReducer = {
-  user: user_slice,
-  teacher: teacher_slice,
-};
-
-export default rootReducer;
